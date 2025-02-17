b8bc9babada8f3bdb175a82a1c517de381c41963
Deepa Dhandapani <deepadhandapani2001@gmail.com>
2025-02-17 15:33:46 +0530

    Update Projects.jsx

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,20 +5,17 @@ const projects = [
   {
     title: "Calculator",
     description:"A modern and responsive calculator built with HTML, CSS, and JavaScript, offering real-time calculations, keyboard support, and a seamless user experience across all devices.",
-    liveLink: "https://example.com",
-    githubLink: "https://github.com/example",
+    githubLink: "https://github.com/ddeepa03/Calculator.git",
   },
   {
     title: "To-Do App",
     description: "An interactive to-do app built with HTML, CSS, and JavaScript, allowing users to add, edit, and delete tasks with ease. Features persistent task storage for seamless user experience.",
-    liveLink: "https://example.com",
-    githubLink: "https://github.com/example",
+    githubLink: "https://github.com/ddeepa03/To_Do.git",
   },
   {
     title: "Quiz App",
     description: "A dynamic quiz app built with React, allowing users to answer multiple-choice questions and view their score at the end. Features state management and a smooth user interface for an engaging experience.",
-    liveLink: "https://example.com",
-    githubLink: "https://github.com/example",
+    githubLink: "https://github.com/ddeepa03/Quiz_App.git",
   },
 ];
 
@@ -30,9 +27,6 @@ const Projects = () => (
         <div key={index} className="project-card">
           <h3>{project.title}</h3>
           <p>{project.description}</p>
-          <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
-            Live Demo
-          </a>
           <a
             href={project.githubLink}
             target="_blank"
